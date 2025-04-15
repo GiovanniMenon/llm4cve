6388abe4c69d808782e4ee27a478b9df8ecf8d58
Menny <giovanni.menon.dev@gmail.com>
2025-04-15 18:21:35 +0300

    Commands Init and LLM init

---
+++ src/cli/cve.go
@@ -0,0 +1,75 @@
+package cli
+
+import (
+	"context"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/ollama"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+var cve = &cobra.Command{
+	Use:   "cve [CVE-ID]",
+	Short: "",
+	Long:  "",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			logrus.Fatalln("Missing CVE-ID argument. Please provide at least one CVE-ID.")
+		}
+		logrus.Infoln("CVE Provided: ", args)
+
+		resp, err := http.Get("https://cve.circl.lu/api/vulnerability/" + args[0])
+		if err != nil {
+			logrus.Fatalln(err)
+		}
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			if err != nil {
+				fmt.Println(err)
+			}
+		}(resp.Body)
+
+		if resp.StatusCode != http.StatusOK {
+			logrus.Fatalln("Failed to fetch CVEs: %s ", resp.Status)
+		}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			logrus.Fatalln(err)
+		}
+
+		llm, err := ollama.New(
+			ollama.WithModel("deepseek-r1:14b"),
+			ollama.WithServerURL(""),
+			ollama.WithSystemPrompt("You are an assistant that receives CVEs and summarize it providing important information about the vulnerability"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		ctx := context.Background()
+		completion, err := llm.Call(ctx, string(body),
+			llms.WithTemperature(0.8),
+			llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+				fmt.Print(string(chunk))
+				return nil
+			}),
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_ = completion
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(cve)
+}
+
+func parseTime(timeStr string) time.Time {
+	t, _ := time.Parse(time.RFC3339Nano, timeStr)
+	return t
+}
