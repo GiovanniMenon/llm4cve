15df06ee60cb8060c940e9ea2e3287a31f94ae1f
Menny <giovanni.menon.dev@gmail.com>
2025-04-26 23:01:59 +0300

    Union

--- src/cli/root.go
+++ src/cli/root.go
@@ -1,20 +1,89 @@
 package cli
 
 import (
+	"context"
 	"fmt"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/ollama"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
+	"regexp"
+	"strings"
 )
 
 var (
 	individual bool
 	verbose    bool
 	rootCmd    = &cobra.Command{
-		Use:   "llm4cve",
+		Use:   "llm4cve [CVE_ID] [CAPEC_ID]",
 		Short: "",
 		Long:  "",
+		Args:  cobra.ArbitraryArgs,
 		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) < 1 {
+				logrus.Fatalln("Missing argument. Please provide at least one argument between CVE-ID and CAPEC-ID.")
+			}
+			var responses []string
+			for _, arg := range args {
+
+				var url string
+
+				switch {
+				case regexp.MustCompile(`^CVE-\d{4}-\d{4,}$`).MatchString(arg):
+					url = "https://cve.circl.lu/api/vulnerability/" + arg
+
+				case regexp.MustCompile(`^CAPEC-\d+$`).MatchString(arg):
+
+					url = "https://cve.circl.lu/api/capec/" + arg
+				default:
+					logrus.Warnf("Skipping invalid ID: %s", arg)
+					continue
+				}
+
+				logrus.Debug(url)
+
+				resp, err := http.Get(url)
+				if err != nil {
+					logrus.Errorf("HTTP request failed for %s: %v", arg, err)
+					continue
+				}
+				defer resp.Body.Close()
+
+				if resp.StatusCode != http.StatusOK {
+					logrus.Errorf("Failed request for %s: %v", arg, resp.Status)
+					continue
+				}
+
+				bodyBytes, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					logrus.Errorf("Failed reading response body for %s: %v", arg, err)
+					continue
+				}
+
+				responses = append(responses, string(bodyBytes))
+
+			}
+			llm, err := ollama.New(
+				ollama.WithModel("deepseek-R1:14B"),
+				ollama.WithServerURL(""),
+				ollama.WithSystemPrompt(`You are an assistant that receives CVEs and CAPECs and summarizes them,
+				providing important information about this in a complete summary.`),
+			)
+			if err != nil {
+				log.Fatal(err)
+			}
+			ctx := context.Background()
+			_, err = llm.Call(ctx, string(strings.Join(responses, "\n")),
+				llms.WithTemperature(0.8),
+				llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+					fmt.Print(string(chunk))
+					return nil
+				}),
+			)
 
 		},
 	}
